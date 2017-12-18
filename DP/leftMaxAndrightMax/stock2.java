a22f4ddf79c9b8bdeeb1b4a0e9761d03080bef25
Qiaomu Yao <wilsonyqm@gmail.com>
2017-12-17 16:14:21 -0800

    leftMaxAndRightMax

---
+++ DP/leftMaxAndrightMax/stock2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1) return 0;
+        int res = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            res += Math.max(0, prices[i + 1] - prices[i]);
+        }
+        return res;
+    }
+}
